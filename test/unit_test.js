7512f5e2c432711a7e44360ae7a92b687f2fc281
Alice Hong <alicehon@andrew.cmu.edu>
2023-10-12 18:13:16 -0400

    Modified tests.

--- test/unit_test.js
+++ test/unit_test.js
@@ -3,9 +3,9 @@
 const assert = require('assert');
 
 describe('Anonymous Post Feature', () => {
-  // before (function () {
-  //   // create a new user
-  // });
+  before (function () {
+    // create a new user
+  });
 
   it('Fake Passing Unit Test', async () => {
     assert.equal(true, true);
